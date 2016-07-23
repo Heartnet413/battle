3ee91d6b0878c2f6e5b7fa48efd793ee00c72d5f
上田すなお <uedasunao@uedasunaonoAir.lan>
2016-07-23 14:55:13 +0900

    パスの設定をしなおした

--- js/script.js
+++ js/script.js
@@ -11,23 +11,11 @@ function(){
   
   $('.right').hover(function() {
   
-<<<<<<< HEAD
-  $('.right').hover(function() {
-  
-  
-=======
-  
->>>>>>> abd5d033624d596f39f15bd987f3c7dab992cd7e
   $(this).find('.akueriseibunn').fadeIn();
 
 },
 
 function(){
 	 $(this).find('.akueriseibunn').fadeOut();
-<<<<<<< HEAD
 });
 
-  
-=======
-});
->>>>>>> abd5d033624d596f39f15bd987f3c7dab992cd7e
