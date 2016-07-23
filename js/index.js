de65ce169d37196773b2bc658ab53199d51f3b61
江口友章 <heartnet.413@icloud.com>
2016-07-23 17:57:47 +0900

    スクロールイン設置

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 
 $(function(){
 
-	$("#top-title").css({opacity:'0.0'}).animate({opacity:'1'},3000);
+	$("#top-title").css({opacity:'0.0'}).animate({opacity:'1'},5000);
 	// $("#top-sentence").animate({opacity:'1'},fadeSpeed);
-	$(window).scroll(function(){
-		$("#top-sentence").css({opacity:'0.0'}).animate({opacity:'1'},fadeSpeed)
-	});
+	//$(window).scroll(function(){
+		//$("#top-sentence").css({opacity:'0.0'}).animate({opacity:'1'},fadeSpeed)
+	//});
     });
 	// $("#top-btn").hover(
 	// 	function(){
@@ -13,3 +13,14 @@ $(function(){
 
 	// 	},
 	
+var scrollFadein = $('.js-scrollFadein');
+$(window).on('scroll load', function() {
+
+    var scrollTop = $(window).scrollTop();
+    scrollFadein.each(function(i, elm) {
+        var targetPos = $(this).offset().top;
+        if (scrollTop > targetPos - $(window).height() + 100 ) {
+            $(this).animate({ opacity: 1 }, 500);
+        }
+    });
+});
