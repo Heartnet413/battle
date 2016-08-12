2eb5d61e3942f5c10627b5ff3575ed7856f4f28c
江口友章 <heartnet.413@icloud.com>
2016-08-12 17:12:23 +0900

    サイトの移り変わり

--- js/script.js
+++ js/script.js
@@ -19,3 +19,32 @@ function(){
 	 $(this).find('.east-sentence ').fadeOut();
 });
 
+$('.btnleft').click(function(){
+    //console.log('btnleftをクリックした');
+    $('.box').fadeOut(1000);
+    //$('.right').animate({
+    	//'top': '100vh'
+    //}, 1500);
+});
+
+
+$('.btnleft').click(function(){
+    $('.left').slideDown(1000);
+});
+
+
+$('.btnleft').click(function(){
+    $('.right').slideDown(1000);
+});
+
+
+
+
+
+
+
+
+
+
+
+
