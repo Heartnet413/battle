8117ae586d09c2bfb7f7d37542741e96af8905f6
上田すなお <uedasunao@uedasunaonoAir.lan>
2016-08-13 16:20:29 +0900

    投票ボタンの実装

--- js/poll.js
+++ js/poll.js
@@ -1,15 +1,35 @@
 var milkcocoa = new MilkCocoa('hotirsqj107.mlkcca.com');
 
-var ds1 = milkcocoa.dataStore('poll');
+var ds1 = milkcocoa.dataStore('round1');
+var ds2 = milkcocoa.dataStore('round2');
+var ds3 = milkcocoa.dataStore('round3');
+var ds4 = milkcocoa.dataStore('round4');
 
 $(function(){
 	$(".btnleft").click(function(){
-		ds1.push({poll: 'west'});
+		if(round==1){
+			ds1.push({poll: 'west'});
+		} else if(round==2){
+ 			ds2.push({poll: 'west'});           
+		} else if(round==3){
+			ds3.push({poll: 'west'});
+		} else if (round==4){
+			ds4.push({poll: 'west'});
+		}
  	});
 
  	$(".btnright").click(function(){
-		ds1.push({poll: 'east'});
+		if(round==1){
+			ds1.push({poll: 'east'});
+		} else if(round==2){
+ 			ds2.push({poll: 'east'});           
+		} else if(round==3){
+			ds3.push({poll: 'east'});
+		} else if (round==4){
+			ds4.push({poll: 'east'});
+		}
  	});
 
 });
 
+console.log(round);
