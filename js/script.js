f36b3ae8fd961dc4de246db32077444d1d321ebd
松本 和馬 <hello@mtmtkzm.com>
2016-08-16 09:54:08 +0900

    fadein時間の調整

--- js/script.js
+++ js/script.js
@@ -18,78 +18,75 @@ $('.right').hover(
 );
 
 
-// フェードインアニメーション
-function fadeinAnime () {
-  // $('.now').fadeOut(1000);
-  $('.next').fadeIn(2000);
-}
-
-
 var round = 1;
 // ボタんクリック
 $('.btn').click(function(){
-  console.log(round);
   if (round==1) {
 
     $('.drink').find('.box.left').animate({
       'left': '-50vw'
-    }, 1000);
+    }, 2000);
 
     $('.drink').find('.box.right').animate({
       'right': '-50vw'
-    }, 1000);
+    }, 2000);
+
+    $('.cup').fadeIn(2000);
 
     setTimeout(function() {
       $('.drink').hide();
-    }, 1000);
+    }, 2000);
 
-    $('.cup').fadeIn(2000);
+    
 
   } else if(round==2) {
 
     $('.cup').find('.box.left').animate({
       'left': '-50vw'
-    }, 1000);
+    }, 2000);
 
     $('.cup').find('.box.right').animate({
       'right': '-50vw'
-    }, 1000);
+    }, 2000);
+
+    $('.hair').fadeIn(2000);
 
     setTimeout(function() {
       $('.cup').hide();
-    }, 1000);
+    }, 2000);
 
-    $('.hair').fadeIn(2000);
+    
 
   } else if (round==3) {
 
     $('.hair').find('.box.left').animate({
       'left': '-50vw'
-    }, 1000);
+    }, 2000);
 
     $('.hair').find('.box.right').animate({
       'right': '-50vw'
-    }, 1000);
+    }, 2000);
+
+    $('.cake').fadeIn(2000);
 
     setTimeout(function() {
       $('.hair').hide();
-    }, 1000);
+    }, 2000);
 
-    $('.cake').fadeIn(2000);
+    
 
   } else if(round==4) {
     $('.cake').find('.box.left').animate({
       'left': '-50vw'
-    }, 1000);
+    }, 2000);
 
     $('.cake').find('.box.right').animate({
       'right': '-50vw'
-    }, 1000);
+    }, 2000);
 
     setTimeout(function() {
       $('.cake').hide();
-    }, 1000);
-
+    }, 2000);
 
   }
 
