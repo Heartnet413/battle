3b52f1f81c36250ea621c37fa6bf7772c880c06c
上田すなお <uedasunao@uedasunaonoAir.lan>
2016-08-16 14:08:49 +0900

    いろいろ

--- js/index.js
+++ js/index.js
@@ -2,16 +2,8 @@
 $(function(){
 
 	$("#top-title").css({opacity:'0.0'}).animate({opacity:'1'},5000);
-	// $("#top-sentence").animate({opacity:'1'},fadeSpeed);
-	//$(window).scroll(function(){
-		//$("#top-sentence").css({opacity:'0.0'}).animate({opacity:'1'},fadeSpeed)
-	//});
     });
-	// $("#top-btn").hover(
-	// 	function(){
-	// 		$(this).css('background-color','#24d')
-
-	// 	},
+	
 	
 var scrollFadein = $('.js-scrollFadein');
 $(window).on('scroll load', function() {
